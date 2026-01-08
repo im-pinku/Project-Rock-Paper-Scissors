0d18e7a2670d5b474c285a04690b99ac8689703e
im-pinku <225466661+im-pinku@users.noreply.github.com>
2026-01-08 10:53:16 +0700

    project Rock Paper Scissors !
    first upload of project rock paper scissor
    player can choose rock paper and scissor
    cmoputer random also work
    player's random also work
    
    need to work on
    scoring system and
    games round system

---
+++ index.js
@@ -0,0 +1,55 @@
+getRandomInt = (max) => Math.floor(Math.random() * max);
+// let getPlayerChoice = String(prompt("enter ur choice \n rock \n paper \n scissor \n or  else will random ")).toLowerCase;
+
+ function Get_random(x){
+    let y =getRandomInt(x);
+    console.log(y)
+    return y;
+ }
+
+   let round = 0;
+ 
+ function get_playerChoice (){ 
+   let x = prompt("enter ur choice \n rock \n paper \n scissor \n or  else will random ");
+
+   console.log(x.toLowerCase());
+   if (x.toLowerCase() =="rock"){
+      return 0;
+   }else if(x.toLowerCase() =="papper"){
+      return 1;
+   }else if(x.toLowerCase() =="scissor"){
+      return 2;
+   }else {
+      console.log('randomming');
+      return Get_random(3);
+   }
+ }
+
+
+ function logic ( ){
+   const x = get_playerChoice();
+   const y = Get_random(3);
+   
+   if ( x === y){
+      console.log("draw");   
+      
+      
+   }else if (  x == 0 && y == 2 ||
+               x == 1 && y == 0 ||
+               x == 2 && y == 1  ){
+               console.log("player win");  
+               
+         
+   }else{
+      console.log("computer win"); 
+   
+   } 
+ }
+  
+ function games (){  
+   logic()
+ }
+ 
+
+ //if highscore > (total round /2 ) game end
+ 
